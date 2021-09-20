0a2192f6d377d1388dfcbce9a01aa48ef1bfb62b
Adithya M Dileep <73463333+Adithya-M-Dileep@users.noreply.github.com>
2021-09-20 17:40:52 +0530

    separate functioning

--- gameDevProject/Assets/scripts/gunInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="fps/new Gun")]
-public class gunInfo : itemInfo
-{
-    public float damage;
-}
